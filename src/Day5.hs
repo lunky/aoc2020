bdd3e6ea1fc8581cde80ab724671a4f61795cac3
Quinn Wilson <github@midworld.org>
2020-12-05 00:59:16 -0600

    day5 done

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,14 +1,43 @@
 module Day5
     (  day5
       ,day5b
+      ,getRow
+      ,getColumn
+      ,getSeatId
     ) where
 
-import Aoc
+import Data.List (sort)
 
 day5 :: String -> Int
-day5 input = 0
+day5 input = maximum $ map getSeatId $ parseInput input
 
 day5b :: String -> Int
-day5b input = 0
+day5b input =  missingSeatCheck $ sort $ map getSeatId $ parseInput input
 
-_input = ""
+getSeatId :: String -> Int
+getSeatId input = getRow input * 8 + getColumn input 
+
+getRow :: String -> Int
+getRow input = head $ foldr (\y acc -> case y of  'F' -> lowerHalf acc 
+                                                  'B' -> upperHalf acc) [0..127] inst 
+  where inst = reverse $ take 7 input
+
+getColumn :: String -> Int
+getColumn input = head $ foldr (\y acc -> case y of  'L' -> lowerHalf acc 
+                                                     'R' -> upperHalf acc) [0..7] inst 
+  where inst = reverse $ drop 7 input
+
+upperHalf :: [Int] -> [Int]
+upperHalf xs = drop (length xs `div` 2) xs
+
+lowerHalf :: [Int] -> [Int]
+lowerHalf xs = take (length xs `div` 2) xs
+
+missingSeatCheck :: [Int] -> Int
+missingSeatCheck xs = (\(x,_)->x+1)$ head $ filter (\(x,y)->y-x>1) $ zip xs (tail xs)
+  
+
+parseInput :: String -> [String]
+parseInput = lines
+
+_input = "FBFBBFFRLR"
--- test/Day5Spec.hs
+++ test/Day5Spec.hs
@@ -5,16 +5,65 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
-    describe "day5" $ 
-        it "should run a noop test" $ 
-            1 `shouldBe` 1
-    describe "day5" $ 
-        it "should do sample 1" $ do
-            let input = ""
-            let expected = 0
-            day5 input `shouldBe` expected
-    describe "day5b" $ 
-        it "should do sample 1" $ do
-            let input = ""
-            let expected = 0
-            day5b input `shouldBe` expected
+  describe "day5" $ 
+    it "should run a noop test" $ 
+      1 `shouldBe` 1
+  describe "day5" $ 
+    it "should do sample 1" $ do
+      let input = "BFFFBBFRRR\nFFFBBBFRRR\nBBFFBBFRLL"
+      let expected = 820
+      day5 input `shouldBe` expected
+  describe "getRow" $ do
+    it "should match pattern 1" $ do 
+      let input = "FBFBBFFRLR"
+      let expected = 44
+      getRow input `shouldBe` expected
+    it "should match pattern 2" $ do 
+      let input = "BFFFBBFRRR"
+      let expected = 70
+      getRow input `shouldBe` expected
+    it "should match pattern 3" $ do 
+      let input = "FFFBBBFRRR"
+      let expected = 14
+      getRow input `shouldBe` expected
+    it "should match pattern 4" $ do 
+      let input = "BBFFBBFRLL"
+      let expected = 102
+      getRow input `shouldBe` expected
+  describe "getColumn" $
+    it "should match pattern 1" $ do 
+      let input = "FBFBBFFRLR"
+      let expected = 5
+      getColumn input `shouldBe` expected
+
+  describe "getSeatId" $ 
+    it "should match pattern 1" $ do 
+      let input = "FBFBBFFRLR"
+      let expected = 357
+      getSeatId input `shouldBe` expected
+
+  describe "getSeatId" $
+    it "BFFFBBFRRR: row 70, column 7, seat ID 567" $ do 
+      let input = "BFFFBBFRRR"
+      let expected = 567
+      getSeatId input `shouldBe` expected
+
+  describe "getSeatId" $ 
+    it "FFFBBBFRRR: row 14, column 7, seat ID 119" $ do 
+      let input = "FFFBBBFRRR"
+      let expected = 119
+      getSeatId input `shouldBe` expected
+
+  describe "BBFFBBFRLL: row 102, column 4, seat ID 820" $ 
+    it "should match pattern 1" $ do 
+      let input = "BBFFBBFRLL"
+      let expected = 820
+      --
+      getSeatId input `shouldBe` expected
+
+
+  describe "day5b" $ 
+    it "should do sample 1" $ do
+      let input = "BFBBBBBRLL\nBFBBBBBRLR\nBFBBBBBRRL\nBBFFFFFLLL\nBBFFFFFLLR\nBBFFFFFLRL\n"
+      let expected = 767
+      day5b input `shouldBe` expected
