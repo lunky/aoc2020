a1bbdcbeb72396eb916aa9e800cdf84b1ed3c5d1
Quinn Wilson <github@midworld.org>
2020-12-08 07:05:14 -0600

    day8 working

--- src/Day8.hs
+++ src/Day8.hs
@@ -6,7 +6,7 @@ module Day8
     where
 
 import Data.List (nub)
-import Data.Either
+import Data.Either (rights)
     
 day8 :: String -> Int 
 day8 input = case bootCode' (parseInput input,0,0,[]) of
@@ -14,7 +14,11 @@ day8 input = case bootCode' (parseInput input,0,0,[]) of
                _ -> error "shouldn't get here"
 
 day8b :: String -> Int
-day8b input = 0
+day8b input = head $ rights $ day8b' input
+
+day8b' input = map (\y-> bootCode' (y ++ [("nop",0::Int)] ,0,0,[])) possible
+  where possible = map (\(i,cmd)->replaceAtIndex i cmd operations) $ nopJmp $ combos operations
+        operations = parseInput input
 
 bootCode' (operations, pointer, acc, history) = do
   let res = case op of
@@ -22,18 +26,23 @@ bootCode' (operations, pointer, acc, history) = do
               "acc" -> (operations, pointer  +1,  acc+arg,   pointer:history)
               "jmp" -> (operations, pointer  +arg, acc,      pointer:history)
               _ -> error "shouldn't get here"
-  if foundVisited res then Left acc else bootCode' res
+  if finished res then Right acc else 
+    if foundVisited res then Left acc else bootCode' res
   where (op,arg) = operations!!pointer
         foundVisited (_,_,_,hist) = length hist /= length (nub hist)
+        finished (_,pointer,_,_) = length operations == pointer
 
 
 nopJmp = map (\(i,(op,arg)) -> if op=="nop" then (i,("jmp",arg)) else (i,("nop",arg))) 
 combos operations = filter (\(_,(op,_)) -> op =="nop" || op=="jmp") $ zip [0..]  operations
 
+replaceAtIndex :: Int -> a -> [a] -> [a]    
+replaceAtIndex i x xs = take i xs ++ [x] ++ drop (i+1) xs
 
 
 
-parseInput input = map (\y->(\[a,b]->(a,cleaned b))$words y)$ lines input
+
+parseInput input = map ((\[a,b]->(a,cleaned b)).words) $ lines input
   where cleaned xs = read (filter (/='+') xs )::Int
 
 _input="nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6\n"
--- test/Day8Spec.hs
+++ test/Day8Spec.hs
@@ -5,13 +5,13 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
-  describe "Day8" $ do
+  describe "Day8" $ 
     it "should do sample 1" $ do
       let input="nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6\n"
       let expected=5
       day8 input `shouldBe` expected
-  describe "Day8b" $ do
-    xit "should do sample 1" $ do
-      let input=""
-      let expected=0
+  describe "Day8b" $ 
+    it "should do sample 1" $ do
+      let input="nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6\n"
+      let expected=8
       day8b input `shouldBe` expected
