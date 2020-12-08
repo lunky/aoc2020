935183ff49786c01b5d8c657af53f3e32f6ab576
Quinn Wilson <github@midworld.org>
2020-12-08 07:05:23 -0600

    clean

--- test/Day7Spec.hs
+++ test/Day7Spec.hs
@@ -23,23 +23,16 @@ spec = do
       day7b input `shouldBe` expected
 
   describe "sumBag" $ do
+    let bags=[("light red",[(1,"bright white"),(2,"muted yellow")]),("dark orange",[(3,"bright white"),(4,"muted yellow")]),("bright white",[(1,"shiny gold")]),("muted yellow",[(2,"shiny gold"),(9,"faded blue")]),("shiny gold",[(1,"dark olive"),(2,"vibrant plum")]),("dark olive",[(3,"faded blue"),(4,"dotted black")]),("vibrant plum",[(5,"faded blue"),(6,"dotted black")])]
     it "should do sample 1" $ do
-      let bags=[("light red",[(1,"bright white"),(2,"muted yellow")]),("dark orange",[(3,"bright white"),(4,"muted yellow")]),("bright white",[(1,"shiny gold")]),("muted yellow",[(2,"shiny gold"),(9,"faded blue")]),("shiny gold",[(1,"dark olive"),(2,"vibrant plum")]),("dark olive",[(3,"faded blue"),(4,"dotted black")]),("vibrant plum",[(5,"faded blue"),(6,"dotted black")])]
       let input = (1,"faded blue")
       let expected = 0
       sumBag bags input `shouldBe` expected
     it "should do sample 2" $ do
-      let bags=[("light red",[(1,"bright white"),(2,"muted yellow")]),("dark orange",[(3,"bright white"),(4,"muted yellow")]),("bright white",[(1,"shiny gold")]),("muted yellow",[(2,"shiny gold"),(9,"faded blue")]),("shiny gold",[(1,"dark olive"),(2,"vibrant plum")]),("dark olive",[(3,"faded blue"),(4,"dotted black")]),("vibrant plum",[(5,"faded blue"),(6,"dotted black")])]
       let input = (1,"vibrant plum")
       let expected = 11 
       sumBag bags input `shouldBe` expected
     it "should do sample 3" $ do
-      let bags=[("light red",[(1,"bright white"),(2,"muted yellow")]),("dark orange",[(3,"bright white"),(4,"muted yellow")]),("bright white",[(1,"shiny gold")]),("muted yellow",[(2,"shiny gold"),(9,"faded blue")]),("shiny gold",[(1,"dark olive"),(2,"vibrant plum")]),("dark olive",[(3,"faded blue"),(4,"dotted black")]),("vibrant plum",[(5,"faded blue"),(6,"dotted black")])]
-      let input = (1,"shiny gold")
-      let expected = 32
-      sumBag bags input `shouldBe` expected
-    it "should do sample 4" $ do
-      let bags=[("light red",[(1,"bright white"),(2,"muted yellow")]),("dark orange",[(3,"bright white"),(4,"muted yellow")]),("bright white",[(1,"shiny gold")]),("muted yellow",[(2,"shiny gold"),(9,"faded blue")]),("shiny gold",[(1,"dark olive"),(2,"vibrant plum")]),("dark olive",[(3,"faded blue"),(4,"dotted black")]),("vibrant plum",[(5,"faded blue"),(6,"dotted black")])]
       let input = (1,"shiny gold")
       let expected = 32
       sumBag bags input `shouldBe` expected
