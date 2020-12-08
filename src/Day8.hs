fa91cec6e8785e655fdacdcdefcdace9fe5e5a0a
Quinn Wilson <github@midworld.org>
2020-12-08 00:26:16 -0600

    Day8 part a done

--- src/Day8.hs
+++ src/Day8.hs
@@ -4,9 +4,36 @@ module Day8
    ,day8b
     )
     where
+
+import Data.List (nub)
+import Data.Either
     
 day8 :: String -> Int 
-day8 input = 0 
+day8 input = case bootCode' (parseInput input,0,0,[]) of
+               Left x -> x
+               _ -> error "shouldn't get here"
 
 day8b :: String -> Int
 day8b input = 0
+
+bootCode' (operations, pointer, acc, history) = do
+  let res = case op of
+              "nop" -> (operations, pointer  +1,  acc,       pointer:history)
+              "acc" -> (operations, pointer  +1,  acc+arg,   pointer:history)
+              "jmp" -> (operations, pointer  +arg, acc,      pointer:history)
+              _ -> error "shouldn't get here"
+  if foundVisited res then Left acc else bootCode' res
+  where (op,arg) = operations!!pointer
+        foundVisited (_,_,_,hist) = length hist /= length (nub hist)
+
+
+nopJmp = map (\(i,(op,arg)) -> if op=="nop" then (i,("jmp",arg)) else (i,("nop",arg))) 
+combos operations = filter (\(_,(op,_)) -> op =="nop" || op=="jmp") $ zip [0..]  operations
+
+
+
+
+parseInput input = map (\y->(\[a,b]->(a,cleaned b))$words y)$ lines input
+  where cleaned xs = read (filter (/='+') xs )::Int
+
+_input="nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6\n"
--- test/Day8Spec.hs
+++ test/Day8Spec.hs
@@ -6,9 +6,9 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "Day8" $ do
-    xit "should do sample 1" $ do
-      let input=""
-      let expected=0
+    it "should do sample 1" $ do
+      let input="nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6\n"
+      let expected=5
       day8 input `shouldBe` expected
   describe "Day8b" $ do
     xit "should do sample 1" $ do
