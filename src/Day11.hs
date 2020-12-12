0db0ad8103e8b18a6341985961370ba318ae6218
Quinn Wilson <github@midworld.org>
2020-12-12 01:24:07 -0600

    working

--- src/Day11.hs
+++ src/Day11.hs
@@ -2,6 +2,8 @@ module Day11
     ( 
     day11
    ,day11b
+   ,sitb
+   ,parseInput
     )
     where
 
@@ -10,12 +12,15 @@ import Data.Map (Map)
 import qualified Data.Map as Map
     
 day11 :: String -> Int 
-day11 input = firstDuplicate $ map (length.filter (=='#').map snd.Map.toList) $ iterate (Day11.round) $ Map.fromList $ parseInput input
+day11 input = firstDuplicate  $ map (Map.size . Map.filter (=='#')) 
+                              $ iterate doRound 
+                              $ Map.fromList 
+                              $ parseInput input
 
 firstDuplicate xs = fst.head.dropWhile (uncurry (/=)) $ (\a->zip a (tail a)) xs 
 
 day11b :: String -> Int
-day11b input = 0
+day11b input = firstDuplicate $ map (Map.size . Map.filter (=='#')) $ iterate doRoundb $ Map.fromList $ parseInput input
 
 
 getXY (x,y) grid = case (find (\(xy,_)->xy==(x,y)) grid) of 
@@ -27,14 +32,44 @@ getXYMap (x,y) grid = Map.lookup (x,y) grid
 adjacent (x,y) grid = map (\y ->getXYMap y grid) [(x-1,y-1),(x,y-1),(x+1,y-1),
                                           (x-1,y  ),        (x+1,y  ),
                                           (x-1,y+1),(x,y+1),(x+1,y+1)]
+visible (x,y) grid = map (\z->(takeWhileOneMore (\y -> y /= Just 'L' && y /= Just '#' )) $ map (\y ->getXYMap y grid) z) $ ways (x,y) grid
+
+ways (x,y) grid = 
+    [
+      --up down
+      reverse [(i,y) | i <- [0..x], i/=x],
+      [(i,y) | i <- [x..width], i/=x],
+      --across
+      reverse [(x,i) | i <- [0..y], i/=y],
+      [(x,i) | i <- [y..width], i/=y],
+      --diagonal down
+      [(x-i,y+i) | i <- [1..width], x-i>=0, y+i>=0, x-i<=width, y+i<=width],
+      [(x+i,y-i) | i <- [1..width], x+i>=0, y-i>=0, x+i<=width, y-i<=width],
+      --diagonal up
+      [(i+x,i+y) | i <- [(-1)*width..width], i+x>x, i+y>y, i+x<=width, i+y<=width ],
+      reverse [(i+x,i+y) | i <- [(-1)*width..width], i+x>=0, i+y>=0, i+x<x, i+y<y]
+    ]
+  where 
+    width = round $ sqrt $ fromIntegral $ Map.size grid
 
 sit (x,y) seat grid
   | seat=='L' && notElem (Just '#') (adjacent (x,y) grid) = '#'
   | seat=='#' && (>=4) (length $ filter (==Just '#') $ adjacent (x,y) grid) = 'L'
   | otherwise = seat
 
+sitb (x,y) seat grid
+  | seat=='L' && (all (==False) $ map (any (==Just '#')) $ visible (x,y) grid) = '#'
+  | seat=='#' && (>=5) (length $ filter (==True) $ map (any (==Just '#')) $ visible (x,y) grid) = 'L'
+  | otherwise = seat
 
-round seats = Map.mapWithKey (\k a ->sit k a seats) seats
+takeWhileOneMore :: (a -> Bool) -> [a] -> [a]
+takeWhileOneMore p = foldr (\x ys -> if p x then x:ys else [x]) []
+
+
+
+doRound seats = Map.mapWithKey (\k a ->sit k a seats) seats
+
+doRoundb seats = Map.mapWithKey (\k a ->sitb k a seats) seats
 
 parseInput input =  concatMap (\(y,row) -> map (\(x,datum)-> ((x,y),datum) )  row)
                       $ zip [0..] $ map (zip [0..])
--- test/Day11Spec.hs
+++ test/Day11Spec.hs
@@ -1,18 +1,32 @@
 module Day11Spec where
 
+import qualified Data.Map as Map
 import Day11
 import Test.Hspec
 
 spec :: Spec
 spec = do
   describe "Day11" $ do
-    xit "should do sample 1" $ do
+    it "should do sample 1" $ do
       let input ="L.LL.LL.LL\nLLLLLLL.LL\nL.L.L..L..\nLLLL.LL.LL\nL.LL.LL.LL\nL.LLLLL.LL\n..L.L.....\nLLLLLLLLLL\nL.LLLLLL.L\nL.LLLLL.LL"
       let expected=37
       day11 input `shouldBe` expected
   describe "Day11b" $ do
-    xit "should do sample 1" $ do
-      let input=""
-      let expected=0
+    it "should do sample 1" $ do
+      let input ="L.LL.LL.LL\nLLLLLLL.LL\nL.L.L..L..\nLLLL.LL.LL\nL.LL.LL.LL\nL.LLLLL.LL\n..L.L.....\nLLLLLLLLLL\nL.LLLLLL.L\nL.LLLLL.LL"
+      let expected=26
       day11b input `shouldBe` expected
+  describe "sitb" $ do
+    it "should not see the #" $ do
+      let input = (0,2)
+      let input2 = 'L'
+      let input3 = Map.fromList $ parseInput "......\n......\n....L#\n......\n......\n......"
+      let expected = '#'
+      sitb input input2 input3 `shouldBe` expected
+    it "should see the #" $ do
+      let input = (0,2)
+      let input2 = 'L'
+      let input3 = Map.fromList $ parseInput "......\n......\n.....#\n......\n......\n......"
+      let expected = 'L'
+      sitb input input2 input3 `shouldBe` expected
       
